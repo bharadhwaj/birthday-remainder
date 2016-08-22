5cb6ab8c4f39daebf8d7c38f209cf821546a2e31
Anil Kumar <mailmeatanil94@gmail.com>
2016-08-22 15:33:42 +0530

    Removed log and tmp files

--- test/mailers/previews/birthday_mailer_preview.rb
+++ test/mailers/previews/birthday_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/birthday_mailer
 class BirthdayMailerPreview < ActionMailer::Preview
-
+	def sample_email_preview
+		BirthdayMailer.send_birthday_mail(Friend.last)
+	end
 end
